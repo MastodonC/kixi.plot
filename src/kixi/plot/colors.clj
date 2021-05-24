b6a83b0a05449dcf5f1086927693ecddedb37470
bld <bruce@mastodonc.com>
2021-05-24 14:12:18 +0100

    MC brand colours

--- src/kixi/plot/colors.clj
+++ src/kixi/plot/colors.clj
@@ -7,6 +7,21 @@
   ([c a]
    (color/color c a)))
 
+;; from Colorgorical http://vrl.cs.brown.edu/color
+;; based on "#29733c", "#50b938", "#59c4b8", "#256cc6", "#fa814c", "#fbe44c"
+(def mc-20-palette (into []
+                         (map color/color)
+                         [;; supplied
+                          "#29733c", "#50b938", "#59c4b8", "#256cc6", "#fa814c", "#fbe44c",
+                          ;; generated
+                          "#7c338b", "#f2c8e8", "#270fe2", "#e18af4", "#474556", "#38b5fc", "#bb15b7", "#92a654", "#b71c41", "#b38491", "#743502", "#ff6b97", "#4e4809", "#ff2a0d"]))
+(def mc-dark-green (color/color "#29733c"))
+(def mc-light-green (color/color "#50b938"))
+(def mc-light-blue (color/color "#59c4b8"))
+(def mc-dark-blue (color/color "#256cc6"))
+(def mc-orange (color/color "#fa814c"))
+(def mc-yellow (color/color "#fbe44c"))
+
 (def orange (nth (color/palette :tableau-20) 2))
 (def blue (nth (color/palette :tableau-20) 5))
 (def green (nth (color/palette :tableau-20) 4))
