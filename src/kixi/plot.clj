0f3cf589ead0c0aab0cb008de23ebe79d6c4f09e
bld <bruce@mastodonc.com>
2021-10-25 13:21:16 +0100

    Add function for a non-0 indexed y-axis

--- src/kixi/plot.clj
+++ src/kixi/plot.clj
@@ -116,6 +116,40 @@
                 (plotr/render-lattice size)
                 (add-watermark watermark))})))
 
+(defn free-y-index [{::series/keys [series legend-spec]
+                     ::keys [x-axis
+                             y-axis
+                             size
+                             legend-label ;; new
+                             legend-font-size
+                             title
+                             watermark]
+                     :or {watermark ""}
+                     :as chart-spec}]
+  (swap! cfg/configuration
+         (fn [c]
+           (-> c
+               (assoc-in [:legend :font] "Open Sans Bold")
+               (assoc-in [:legend :font-size] (or legend-font-size (if (< (count legend-spec) 22) 24 16)))))) ;; 22 Items fit in the legend at 24pt
+  (let [size (or size {:width 1539 :height 1037 :background colors/white}) ;; 1539x1037 is almost exactly the right size to go into the slide
+        title-format (or (::format title) {:font-size 36 :font "Open Sans Bold" :font-style :bold :margin 36})]
+    (merge chart-spec
+           {::canvas
+            (-> (into [[:grid]] series) ;; this is good
+                (plotb/preprocess-series)
+                (plotb/update-scale :x :fmt (::tick-formatter x-axis))
+                (update-chart-x-axis-ticks series)
+                (plotb/update-scale :y :fmt (::tick-formatter y-axis))
+                (update-chart-y-axis-ticks series)
+                (plotb/add-axes :bottom {:ticks {:font-size 24 :font-style nil}})
+                (plotb/add-axes :left {:ticks {:font-size 24 :font-style nil}})
+                (plotb/add-label :bottom (::label x-axis) {:font-size 36 :font "Open Sans" :font-style nil})
+                (plotb/add-label :left (::label y-axis) {:font-size 36 :font "Open Sans" :font-style nil})
+                (plotb/add-label :top (::label title) title-format)
+                (plotb/add-legend legend-label legend-spec)
+                (plotr/render-lattice size)
+                (add-watermark watermark))})))
+
 (defn show [plot]
   (plot/show plot))
 
